0358a6544c88aeebe185e3aa40ec4ed253bdf7a3
wanqijian <wanqijian@outlook.com>
2017-09-21 19:52:51 +0800

    得到首页的链接内容

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
  * @Author            : cnwanq
  * @Date              : 2017-09-20 20: 49: 52
  * @Last Modified by  : cnwanq
- * @Last Modified time: 2017-09-20 21: 44: 52
+ * @Last Modified time: 2017-09-21 19: 51: 51
  */
 
-var request = require('./crawler/request.js');
+// var request = require('./crawler/request.js');
+var request = require('request');
+var cheerio = require('cheerio');
 var fs      = require('fs');
 var url     = require('url');
 
@@ -15,8 +17,19 @@ console.info('Hello Node Crawler');
 var targetUrl = 'http://www.klook.com';
 
 request(targetUrl, function (err, res, data) {
+    var $          = cheerio.load(data);
+    var indexHrefs = new Array();
+    
+    $("a").each(function(index, element) {
+        var href = $(this).attr("href");
+
+        if(href && href.length>0) {
+            indexHrefs[indexHrefs.length] = href;
+        }
+        
+    });
     var targetUrlObj = url.parse(targetUrl);
-    fs.writeFile('./data/'+targetUrlObj.host, data, function(err) {
+    fs.writeFile('./data/'+targetUrlObj.host+'-allhrefs', indexHrefs.join('\n'), function(err) {
         if (err) {
             throw err;
         }
